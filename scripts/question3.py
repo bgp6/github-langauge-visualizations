4fea457aa28036875603e8f8a5c876461dceea92
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2023-12-12 22:45:14 +0000

    Ugly but working solution for question 3

---
+++ scripts/question3.py
@@ -0,0 +1,23 @@
+import pathlib
+import pandas as pd
+import matplotlib.pyplot as plt
+
+ROOT = pathlib.Path(__file__).parent.parent.resolve()
+OUTPUT = ROOT.joinpath("output/")
+OUTPUT.mkdir(exist_ok=True)
+DATA_FILE = ROOT.joinpath("data/combined_data.csv")
+
+# Generate combined data if it does not exist
+if not DATA_FILE.is_file():
+    import preprocess
+
+LANGUAGES = ["JavaScript", "Python", "Ruby", "Java", "PHP", "C", "C++", "Objective-C", "C#", "Perl", "Go", "CoffeeScript", "TypeScript", "Assembly", "Swift", "Scala", "Lua", "Haskell", "R", "Clojure", "Objective-C++", "Rust", "Erlang", "OCaml", ]
+
+data = pd.read_csv(DATA_FILE)
+data = data.query('year == 2023 and quarter == 3 and name in @LANGUAGES')
+data = data.sort_values(by=["repo"], ascending=False)
+data["issues_per_repo"] = data["issue"] / data["repo"]
+plt.bar(data["name"], data["issues_per_repo"])
+plt.savefig(OUTPUT.joinpath("issues_per_repo.svg"))
+
+print(data)
