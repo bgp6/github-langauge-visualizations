f7c25442bbf88d97ed29fa449f63f6ebcbe8984b
brianzjk <70183960+brianzjk@users.noreply.github.com>
2023-12-12 14:15:35 -0500

    adding comments
    better code readability is usually a good thing

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -2,56 +2,35 @@ import pandas as pd
 
 ROOT = "../data/{}"
 
-# reading in data
-issue = pd.read_csv(ROOT.format("gh-issue-event.csv"))
-pull = pd.read_csv(ROOT.format("gh-pull-request.csv"))
-push = pd.read_csv(ROOT.format("gh-push-event.csv"))
-star = pd.read_csv(ROOT.format("gh-star-event.csv"))
-repo = pd.read_csv(ROOT.format("gh-repos-with-language.csv"))
+data = pd.read_csv(ROOT.format("combined_data.csv"))
 
-# Add dummy year and quarter to repos count data
-repo["year"] = "2023"
-repo["quarter"] = "3"
-
-# creating an uid (unique identifier) for each entry so we can combine the tables
-def add_uid(df, dtype):
-    uid = []
+# returns list with normalized values
+def get_normal(df, dtype):
+    normal = []     # stores date range and value
+    max_val = {}    # max val of that date range 
     for index, row in df.iterrows():
-        uid.append(row["name"] + "__" + str(row["year"]) + "__" + str(row["quarter"]))
-    df["uid"] = uid
-    df.set_index("uid", inplace = True)
-    df.rename(columns = {"count" : dtype}, inplace = True)
-    df.drop(columns = ["name", "year", "quarter"], inplace = True)
-    return df
+        time = str(row["year"]) + "_" + str(row["quarter"])
+        if time in max_val:
+            max_val[time] = max(max_val[time], row[dtype])
+        else:
+            max_val[time] = row[dtype]
+        normal.append((time, row[dtype]))
 
-issue = add_uid(issue, "issue")
-pull = add_uid(pull, "pull")
-push = add_uid(push, "push")
-star = add_uid(star, "star")
-repo = add_uid(repo, "repo")
+    # scaling each data point from 0 to 1 
+    for i in range(len(normal)):
+        normal[i] = float(normal[i][1]) / float(max_val[normal[i][0]])
 
-# combining the dataframes
-frames = [issue, pull, push, star, repo]
-ret = pd.concat(frames, axis = 1)
+    return normal
 
-# imputing NaNs in dataframe
-print("Number of NaN:", ret.isna().sum().sum())
-ret.fillna(0, inplace = True)
+data["norm_issue"] = get_normal(data, "issue")
+data["norm_pull"] = get_normal(data, "pull")
+data["norm_push"] = get_normal(data, "push")
+data["norm_star"] = get_normal(data, "star")
 
-# extracting name, year, quarter from uid 
-name = []
-year = []
-quarter = []
+# calculating average of 4 normalized data points 
+total = []
+for index, row in data.iterrows():
+    total.append((row["norm_issue"] + row["norm_pull"] + row["norm_push"] + row["norm_star"]) / 4)
+data["norm_use"] = total
 
-for index, row in ret.iterrows():
-    properties = index.split("__")
-    name.append(properties[0])
-    year.append(properties[1])
-    quarter.append(properties[2])
-    
-ret["name"] = name
-ret["year"] = year
-ret["quarter"] = quarter
-
-# saving file
-ret.to_csv(ROOT.format("combined_data.csv"))
+data.to_csv(ROOT.format("normalized_data.csv"))
