0983a8c78f21ff75ed2231aaca45d4cc79e830ac
brianzjk <70183960+brianzjk@users.noreply.github.com>
2023-12-12 14:19:36 -0500

    adding comments
    better code readability is usually a good thing

--- scripts/normalizing.py
+++ scripts/normalizing.py
@@ -4,9 +4,10 @@ ROOT = "../data/{}"
 
 data = pd.read_csv(ROOT.format("combined_data.csv"))
 
+# returns list with normalized values
 def get_normal(df, dtype):
-    normal = []
-    max_val = {}
+    normal = []     # stores date range and value
+    max_val = {}    # max val of that date range 
     for index, row in df.iterrows():
         time = str(row["year"]) + "_" + str(row["quarter"])
         if time in max_val:
@@ -15,6 +16,7 @@ def get_normal(df, dtype):
             max_val[time] = row[dtype]
         normal.append((time, row[dtype]))
 
+    # scaling each data point from 0 to 1 
     for i in range(len(normal)):
         normal[i] = float(normal[i][1]) / float(max_val[normal[i][0]])
 
@@ -25,6 +27,7 @@ data["norm_pull"] = get_normal(data, "pull")
 data["norm_push"] = get_normal(data, "push")
 data["norm_star"] = get_normal(data, "star")
 
+# calculating average of 4 normalized data points 
 total = []
 for index, row in data.iterrows():
     total.append((row["norm_issue"] + row["norm_pull"] + row["norm_push"] + row["norm_star"]) / 4)
