b2d1c123933576b94b19bc0534755bcdf8ce9be5
brianzjk <brianzjk@gmail.com>
2023-12-12 20:45:03 -0500

    combined normalizing and preprocess

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -54,5 +54,35 @@ ret["name"] = name
 ret["year"] = year
 ret["quarter"] = quarter
 
+# returns list with normalized values
+def get_normal(df, dtype):
+    normal = []     # stores date range and value
+    max_val = {}    # max val of that date range 
+    for index, row in df.iterrows():
+        time = str(row["year"]) + "_" + str(row["quarter"])
+        if time in max_val:
+            max_val[time] = max(max_val[time], row[dtype])
+        else:
+            max_val[time] = row[dtype]
+        normal.append((time, row[dtype]))
+
+    # scaling each data point from 0 to 1 
+    for i in range(len(normal)):
+        normal[i] = float(normal[i][1]) / float(max_val[normal[i][0]])
+
+    return normal
+
+# adding normalized columns to the dataframe
+ret["norm_issue"] = get_normal(ret, "issue")
+ret["norm_pull"] = get_normal(ret, "pull")
+ret["norm_push"] = get_normal(ret, "push")
+ret["norm_star"] = get_normal(ret, "star")
+
+# calculating average of 4 normalized data points 
+total = []
+for index, row in ret.iterrows():
+    total.append((row["norm_issue"] + row["norm_pull"] + row["norm_push"] + row["norm_star"]) / 4)
+ret["norm_use"] = total
+
 # saving file
 ret.to_csv(ROOT.joinpath("combined_data.csv"))
