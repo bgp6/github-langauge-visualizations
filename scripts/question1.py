091ca9e4458fbfeef51ceaab8f8c1913b43ecb15
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2023-12-18 23:23:47 +0000

    Adapt original question 1 script: Minor changes and cleanup Added output

--- scripts/question1.py
+++ scripts/question1.py
@@ -1,108 +1,76 @@
-"""Margaret Jackson
-December 8, 2023
-BGP Group 6 Question 1
-How do the different languages used on Github compare in usage to each other? (pie graph)
-usage = (pulls + stars + pushes) / 3
-expressed as exploding pie chart"""
-
-"""comparing pushes/pulls/etc to the repo data"""
+import pathlib
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-"""df = pd.read_csv(r"combined_data.csv")
-df2 = pd.read_csv(r"total_repos.csv")
-bigList = df['language'].tolist()
-littleList = df2['name'].tolist()
+ROOT = pathlib.Path(__file__).parent.parent.resolve()
+OUTPUT = ROOT.joinpath("output/")
+OUTPUT.mkdir(exist_ok=True)
+DATA_FILE = ROOT.joinpath("data/combined_data.csv")
 
-totalList = []
-bL = list(set(bigList))
-for i in littleList :
-    for j in bL :
-        if i == j :
-            totalList.append(i)
-print(totalList)
-
-with open('test.csv', 'wb') as f:
-    writer = csv.writer(f)
-    for val in test_list:
-        writer.writerow([val])"""
-
-df = pd.read_csv(r"combdata.csv")
-reponum = df['repo'].tolist()
-namenum = df['name'].tolist()
-repo = []
-name = []
-"""isolating the data"""
-for i in range (0, len(reponum)) :
-    if reponum[i] > 0 :
-        repo.append(reponum[i])
-        name.append(namenum[i])
-#print(repo)
-#print(name)
+df = pd.read_csv(DATA_FILE)
+# Drop unused columns
+df = df.drop(columns = ["uid", "year", "quarter", "issue", "pull", "push", "star", "norm_issue", "norm_pull", "norm_push", "norm_star", "norm_use"])
+# Drop rows with no repo count, then sort
+df = df[df.repo > 0]
+df = df.sort_values(by = "repo", ascending=False)
 
 """now it is time for data analysis"""
-#use repo
-#use name
-minnum = int(repo[0] * .05)
-"""print(minnum)
-#counting the number of wedges
-wedcount = 1
-for i in repo :
-    if i >= minnum :
-        wedcount += 1
-        print (i)
-print(wedcount)"""
-#20 wedges at the 0.1 cutoff
-"""creating the data for wedges"""
-reponum.clear()
-namenum.clear()
-count = 0
-for i in range (0,20) :
-    reponum.append(repo[i])
-    namenum.append(name[i])
-for i in range(20,len(repo)) :
-    count += repo[i]
-reponum.append(count)
-namenum.append("Other")
-print(reponum)
-print(namenum)
+# Threshold for the `Other` category
+minnum = int(df.repo.max() * 0.25)
 
-#making the graph
-# Creating explode data
-explode = (0.2, 0.1, 0.2, 0.1, 0.0, 0.15, 0.2, 0.3, 0.2, 0.0, 0.15, 0.1, 0.2, 0.25, 0.1, 0.2, 0.0, 0.1, 0.15, 0.1, 0.15)
- 
+# Counting the number of wedges
+wedcount = df[df.repo >= minnum].size + 1
+
+"""creating the data for wedges"""
+# Select rows that will not be grouped into other (normal wedges)
+wedgeData = df[:wedcount-1]
+# Sum repo counts for all languages in `Other` (all remaining languages)
+otherCount = df[wedcount:].repo.sum()
+# Construct a row for the `Other` wedge and append
+otherData = pd.Series({'name': "Other", 'repo': otherCount}).to_frame().T
+wedgeData = pd.concat([wedgeData, otherData], ignore_index = True)
+
+# Making the graph
+# Explosion distance for each wedge
+explode = (0.1, 0.15, 0.1, 0.15, 0.05, 0.15, 0.2, 0.3, 0.2, 0.0, 0.15, 0.1, 0.2, 0.25, 0.1, 0.2, 0.0, 0.1, 0.15, 0.1, 0.15)
+
 # Creating color parameters
-colors = ("steelblue","palevioletred","tomato","cyan","violet","coral","lightpink","gold","deeppink","turquoise","plum","crimson","cornflowerblue","cyan", "mediumseagreen","lightsalmon","hotpink","mediumorchid","pink","paleturquoise","wheat")
+colors = ("deeppink","turquoise","plum","crimson","cornflowerblue","turquoise", "mediumseagreen","lightsalmon","hotpink","mediumorchid","pink","paleturquoise","wheat", "steelblue","palevioletred","mediumslateblue","cyan","violet","coral","lightpink","gold")
 
 # Wedge properties
 wp = { 'linewidth' : 1, 'edgecolor' : "darkblue" }
- 
+# Collect percentages for the legend
+perc = []
 # Creating autocpt arguments
 def func(pct, allvalues):
     absolute = int(pct / 100.*np.sum(allvalues))
-    return "{:.1f}%\n({:d} )".format(pct, absolute) 
+    perc.append("{:.1f}%".format(pct, absolute))
+    return ""
 
 # Creating plot
 fig, ax = plt.subplots(figsize =(10, 7))
-wedges, texts, autotexts = ax.pie(reponum, 
-                                  autopct = lambda pct: func(pct, reponum),
+wedges, texts, autotexts = ax.pie(wedgeData.repo, 
+                                  autopct = lambda pct: func(pct, wedgeData.repo),
                                   explode = explode, 
-                                  labels = namenum,
                                   shadow = True,
                                   colors = colors,
                                   startangle = 90,
                                   wedgeprops = wp,
-                                  textprops = dict(color ="magenta"))
- 
-# Adding legend
-ax.legend(wedges, namenum,
+                                  textprops = dict(color = "darkblue"))
+# Construct legend labels
+labels = []
+for i in range(0, wedgeData.name.size):
+    labels.append(f"{wedgeData.name[i]} {perc[i]}")
+
+# Add legend
+ax.legend(wedges, labels,
           title ="Ages",
           loc ="center left",
           bbox_to_anchor =(1, 0, 0.5, 1))
  
 plt.setp(autotexts, size = 8, weight ="bold")
-ax.set_title("Customizing pie chart")
- 
-# show plot
-plt.show()
+ax.set_title("Percentage of Repositories on Github by Language")
+
+plt.savefig(OUTPUT.joinpath("question_1.svg"), bbox_inches="tight")
+plt.savefig(OUTPUT.joinpath("question_1.png"), bbox_inches="tight")
