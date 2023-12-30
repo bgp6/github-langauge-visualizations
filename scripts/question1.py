149645cf7c6fceda7b34c134c96c10b3c09726c5
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2023-12-30 14:17:44 -0500

    Try to tidy up pie chart; More minimalist

--- scripts/question1.py
+++ scripts/question1.py
@@ -39,14 +39,12 @@ otherData = pd.Series({'name': "Other", 'repo': otherCount}).to_frame().T
 wedgeData = pd.concat([wedgeData, otherData], ignore_index = True)
 
 # Making the graph
-# Explosion distance for each wedge
-explode = (0.1, 0.15, 0.1, 0.15, 0.05, 0.15, 0.2, 0.3, 0.2, 0.0, 0.15, 0.1, 0.2, 0.25, 0.1, 0.2, 0.0, 0.1, 0.15, 0.1, 0.15)
 
 # Creating color parameters
 colors = ("deeppink","turquoise","plum","crimson","cornflowerblue","turquoise", "mediumseagreen","lightsalmon","hotpink","mediumorchid","pink","paleturquoise","wheat", "steelblue","palevioletred","mediumslateblue","cyan","violet","coral","lightpink","gold")
 
 # Wedge properties
-wp = { 'linewidth' : 1, 'edgecolor' : "darkblue" }
+wp = { 'linewidth' : 1, 'edgecolor' : "white", 'antialiased' : True }
 # Collect percentages for the legend
 perc = []
 # Creating autocpt arguments
@@ -59,12 +57,10 @@ def func(pct, allvalues):
 fig, ax = plt.subplots(figsize =(10, 7))
 wedges, texts, autotexts = ax.pie(wedgeData.repo, 
                                   autopct = lambda pct: func(pct, wedgeData.repo),
-                                  explode = explode, 
-                                  shadow = True,
                                   colors = colors,
                                   startangle = 90,
                                   wedgeprops = wp,
-                                  textprops = dict(color = "darkblue"))
+                                  )
 # Construct legend labels
 labels = []
 for i in range(0, wedgeData.name.size):
@@ -72,12 +68,9 @@ for i in range(0, wedgeData.name.size):
 
 # Add legend
 ax.legend(wedges, labels,
-          title ="Ages",
-          loc ="center left",
-          bbox_to_anchor =(1, 0, 0.5, 1))
+          title = "Languages",
+          loc = "center left",
+          bbox_to_anchor = (1, 0, 0.5, 1))
  
-plt.setp(autotexts, size = 8, weight ="bold")
-ax.set_title("Percentage of Repositories on Github by Language")
-
 plt.savefig(OUTPUT.joinpath("question_1.svg"), bbox_inches="tight", transparent = True)
 plt.savefig(OUTPUT.joinpath("question_1.png"), bbox_inches="tight", transparent = True)
