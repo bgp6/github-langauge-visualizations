c41e5b7ae1bd820afec21b391fec4318542dcf1f
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2023-12-30 14:26:18 -0500

    Remove languages from question 2 with 0 issues per repo

--- scripts/question2.py
+++ scripts/question2.py
@@ -14,7 +14,7 @@ if not DATA_FILE.is_file():
 # Apply style sheet for dark backgrounds
 plt.style.use('https://github.com/dhaitz/matplotlib-stylesheets/raw/master/pitayasmoothie-dark.mplstyle')
 
-LANGUAGES = ["JavaScript", "Python", "Ruby", "Java", "PHP", "C", "C++", "Objective-C", "C#", "Perl", "Go", "CoffeeScript", "TypeScript", "Assembly", "Swift", "Scala", "Lua", "Haskell", "R", "Clojure", "Objective-C++", "Rust", "Erlang", "OCaml", ]
+LANGUAGES = ["JavaScript", "Python", "Ruby", "Java", "PHP", "C", "C++", "Objective-C", "C#", "Perl", "Go", "TypeScript", "Swift", "Scala", "Lua", "Haskell", "R", "Clojure", "Rust", "Erlang", "OCaml", ]
 
 data = pd.read_csv(DATA_FILE)
 data = data.query('year == 2023 and quarter == 3 and name in @LANGUAGES')
@@ -22,7 +22,7 @@ data["issues_per_repo"] = data["issue"] / data["repo"]
 data = data.sort_values(by=["issues_per_repo"])
 
 fig, ax = plt.subplots()
-fig.set_figheight(8)
+fig.set_figheight(6)
 barh = ax.barh(data["name"], data["issues_per_repo"])
 ax.bar_label(barh, fmt='%.2f', padding = 2)
 
