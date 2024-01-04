7f4ebb7b33fb9bf961f34dc4c1c1914096bb8d71
nfan314 <35156007+nfan314@users.noreply.github.com>
2024-01-04 18:45:11 +0000

    Got the top 10 languages based on normal usage

--- scripts/question3.py
+++ scripts/question3.py
@@ -14,7 +14,18 @@ if not DATA_FILE.is_file():
 df = pd.read_csv(DATA_FILE)
 
 data = {}
-print (df)
-for i in range (len(df)):
-   if data.has_key(df[i]["name"]):
+for index, row in df.iterrows():
+    name = row["name"]
+    usage = row["norm_use"]
+    new_usage = data.get(name, 0) + usage
+    data[name] = new_usage
+
+# sorted_data = sorted(data.items(), key = lambda x:x[1], reverse = True)
+
+sorted_data = pd.DataFrame.from_dict(data, orient='index', columns=['norm_use'])
+sorted_data = sorted_data.sort_values(by = "norm_use", ascending=False)
+top_ten = sorted_data[:10]
+
+print(top_ten)
+
        
